7c99eef813ab9ffe6e5d80168bebbf27196fdf6e
Rob Liston <rob.liston@rapidsilicon.com>
2023-09-15 13:44:46 -0700

    dev snapshot

--- demo/udp.py
+++ demo/udp.py
@@ -1,15 +1,18 @@
 import socket
 import numpy as np
-import matplotlib.pyplot as plt
 import cv2
 import struct
 import argparse
 import os
 import time
+np.set_printoptions(linewidth=250)
 
 parser = argparse.ArgumentParser(formatter_class=argparse.ArgumentDefaultsHelpFormatter)
 parser.add_argument('--dir', help='directory to store images', default='video')
 parser.add_argument('--n', help='number of frames to capture', default=1000, type=int)
+parser.add_argument('--flip', default=False, action='store_true')
+parser.add_argument('--forever', default=False, action='store_true')
+parser.add_argument('--gallery', default=False, action='store_true')
 args = parser.parse_args()
 
 if not os.path.isdir(args.dir):
@@ -28,125 +31,67 @@ sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
 sock.bind((UDP_IP, UDP_PORT))
 print("socket bind complete")
 
-#fig = plt.figure()
-#ax = fig.add_subplot(1,1,1)
-
-#writer = cv2.VideoWriter("output.avi",
-#cv2.VideoWriter_fourcc(*"mp4v"), 8,(640,480))
-
-fb = np.zeros([24,32], dtype=np.int16)
-#fb0 = np.zeros([24,32], dtype=np.uint16)
-#fb1 = np.zeros([24,32], dtype=np.uint16)
-#fb = np.zeros([384,2], dtype=np.uint16)
+fb = np.zeros([24,32], dtype=np.uint8)
+fbraw = np.zeros([24,32], dtype=np.uint8)
 i=0
-#init=True
-#page=0
 t0 = time.time()
+pred0=0
+pred1=0
+pred2=0
+pred3=0
+pred4=0
 while True:
     data, addr = sock.recvfrom(1024) # buffer size is 1024 bytes
-    hdata = struct.unpack('>384H',data[1:])
+    #print('len(data)',len(data))
+
+    if len(data)==768:
+        hdata = struct.unpack('>384H',data)
+    elif len(data)==768+12:
+        hdata = struct.unpack('>384H',data[12:])
+        status = struct.unpack('>H',data[0:2])[0]
+        pred0 = struct.unpack('>h',data[2:4])[0]
+        pred1 = struct.unpack('>h',data[4:6])[0]
+        pred2 = struct.unpack('>h',data[6:8])[0]
+        pred3 = struct.unpack('>h',data[8:10])[0]
+        pred4 = struct.unpack('>h',data[10:12])[0]
+    else:
+        print("ERROR: UDP PACKET LENGTH",len(data))
+
     gdata = [x if x<32768 else x-65536 for x in hdata]
-    #print('type(hdata)',type(hdata),len(hdata))
-    if data[0]==0:
+    gdata = [-128 if x<-128 else x for x in gdata]
+    gdata = [x+128 for x in gdata]
+
+    if len(data)==768+12:
         for r in range(0,12):
             for c in range(0,32):
-                #fb[r,c] = data[1+r*32*2+c*2+1]
-                #fb[r,c] = ((data[1+r*32*2+c*2]<<8) | data[1+r*32*2+c*2+1])
-                fb[r,c] = gdata[r*32+c]
+                fbraw[r,c] = gdata[r*32+c]
+                if args.flip:
+                    fb[23-r,c] = gdata[r*32+c]
+                else:
+                    fb[r,c] = gdata[r*32+c]
     else:
         for r in range(0,12):
             for c in range(0,32):
-                #fb[r+12,c] = data[1+r*32*2+c*2+1]
-                #fb[r+12,c] = ((data[1+r*32*2+c*2]<<8) | data[1+r*32*2+c*2+1])
-                fb[r+12,c] = gdata[r*32+c]
+                fbraw[r+12,c] = gdata[r*32+c]
+                if args.flip:
+                    fb[23-(r+12),c] = gdata[r*32+c]
+                else:
+                    fb[r+12,c] = gdata[r*32+c]
 
-        #fb = fb & 0x00ff
-        #fb = fb.astype(np.uint8)
-        #fb = fb.astype(np.int8)
-        #fb = (fb+128).astype(np.uint8)
-        #fb = (fb-np.min(fb))/(np.max(fb)-np.min(fb))
-        #fb2 = fb0*even_mask + fb1*odd_mask
-        #fb3 = -1*fb2.astype(np.int16)
-        #fb = fb.astype(np.uint16) & 0x00ff;
-        #fb = fb.astype(np.int16)
-        #fb2 = (fb - np.mean(fb))+128
-        fb1 = fb - np.mean(fb)
-        fb2=fb1
-        #fb2 = np.clip(fb1,-512,512)
-        #fb2 = np.clip(fb1,-3*np.std(fb1),3*np.std(fb1))
-        #fb2 = (fb2-np.min(fb2))/(np.max(fb2)-np.min(fb2))
-        #fb2 = fb1 - np.amin(fb1)
-        #fb2 = fb1*(255/(np.mean(fb2)+2*np.std(fb2)))
-        #print('fb min max',np.amin(fb2),np.amax(fb2),np.mean(fb2),np.std(fb2))
-        fb3 = (fb2-np.min(fb2))/(np.max(fb2)-np.min(fb2))
-        #fb3 = np.clip(fb2,0,255)
-        #print('fb histogram',np.histogram(fb))
-        #print('fb mean std',np.mean(fb), np.std(fb))
-        #fb = (fb-np.min(fb))/(np.max(fb)-np.min(fb))
-        #fb=(fb*255).astype(np.uint8)
-        fb4 = (fb3*255).astype(np.uint8)
-        fbout = cv2.resize(fb4, (640,480), interpolation= cv2.INTER_LINEAR)
-        #fb2 = cv2.resize(fb, (640,480), interpolation= cv2.INTER_NEAREST)
+        fb4=fb
+        fbout = cv2.resize(fb4, (640,480), interpolation= cv2.INTER_NEAREST)
+        fbout = cv2.cvtColor(fbout,cv2.COLOR_GRAY2RGB)
+        pred = np.argmax(np.array([pred0,pred1,pred2,pred3,pred4]))
+        #cv2.putText(fbout, 'pred {}'.format(1+pred), (20, 25), cv2.FONT_HERSHEY_PLAIN, 2, (255,0,255), 4)
+        cv2.putText(fbout, 'pred {}'.format(1+pred), (20, 55), cv2.FONT_HERSHEY_PLAIN, 5, (255,0,255), 4)
         cv2.imshow("frame", fbout)
         cv2.waitKey(1)
         i+=1
+        if args.gallery:
+            cv2.imwrite('gallery/frame_{:05d}.jpg'.format(i), fbout)
         if i<args.n:
-            cv2.imwrite('{}/frame{:05d}.jpg'.format(args.dir,i),fb4)
-            #writer.write(fb5.astype('uint8'))
+            cv2.imwrite('{}/frame{:05d}.jpg'.format(args.dir,i),fbraw)
         elif i==args.n:
             print("dir {} frames {} elapsed {} fps {}".format(args.dir,args.n,time.time()-t0, args.n/(time.time()-t0)))
-            exit()
-
-
-#    if init and len(data)==770 and (data[1]&1)==0:
-#        init=False
-#    else:
-#        continue
-
-    if len(data)==770:
-        #print("status",hex(data[0]),hex(data[1]))
-        if (data[1]&1)==0:
-            page=0
-            for r in range(0,12):
-                for c in range(0,32):
-                    fb0[r,c] = ((data[2+r*32*2+c*2]<<8) | data[2+r*32*2+c*2+1])
-        else:
-            page=1
-            for r in range(0,12):
-                for c in range(0,32):
-                    fb1[r,c] = ((data[2+r*32*2+c*2]<<8) | data[2+r*32*2+c*2+1])
-        #print("page",page)
-    elif len(data)==768:
-        if page==0:
-            for r in range(0,12):
-                for c in range(0,32):
-                    fb0[r+12,c] = ((data[r*32*2+c*2]<<8) | data[r*32*2+c*2+1])
-        else:
-            for r in range(0,12):
-                for c in range(0,32):
-                    fb1[r+12,c] = ((data[r*32*2+c*2]<<8) | data[r*32*2+c*2+1])
-
-            fb2 = fb0*even_mask + fb1*odd_mask
-            #fb3 = -1*fb2.astype(np.int16)
-            fb3 = fb2.astype(np.uint16) & 0x00ff;
-            #print('fb3 min max',np.amin(fb3),np.amax(fb3))
-            fb4 = (fb3-np.min(fb3))/(np.max(fb3)-np.min(fb3))
-            #fb4=fb3.astype(np.uint8)
-            fb5 = cv2.resize(fb4, (640,480), interpolation= cv2.INTER_LINEAR)
-            cv2.imshow("frame", fb5)
-            cv2.waitKey(1)
-            i+=1
-            if i<100:
-                cv2.imwrite('video/video{:05d}.jpg'.format(i),fb5)
-                #cv2.imwrite('video/video{:05d}.jpg'.format(i),(fb5*255).astype(np.uint8))
-                #writer.write(fb5.astype('uint8'))
-            elif i==100:
-                print("finished video writer")
-            #    writer.release()
-                
-            #ax.clear()
-            #ax.matshow(fb)
-            #ax.set_title(f"frame "+str(i))
-            # Note that using time.sleep does *not* work here!
-            #plt.pause(0.01)
-    #print("received message len=",len(data))
+            if not args.forever:
+                exit()
